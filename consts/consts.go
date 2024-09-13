f9af57c438657458d2c850030a01c80dd2ac5629
Niki22n <96103985+Niki22n@users.noreply.github.com>
2024-09-13 14:46:06 +0530

    Update consts.go

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "nikita"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "nkta"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "NKTA"
 )
 
 var ID ids.ID
